e4758a2f100d35549bd437ee15aec17e1d6e94a8
guilherme <ghma.em1@gmail.com>
2023-05-23 22:38:03 -0300

    Fix: arruma configuração do docker

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -3,9 +3,9 @@ import 'dotenv/config';
 
 const config: Options = {
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'password',
+  password: 'password',
   database: process.env.DB_NAME || 'cbc_api',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOSTNAME || 'db',
   port: Number(process.env.DB_PORT) || 3306,
   dialect: 'mysql',
 };
