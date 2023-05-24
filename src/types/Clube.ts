da07486fe0116721d722496f4b2e9416f2708d2e
guilherme <ghma.em1@gmail.com>
2023-05-23 21:00:22 -0300

    Cria tipos para as tabelas

---
+++ src/types/Clube.ts
@@ -0,0 +1,5 @@
+export type Clube = {
+  id: number,
+  clube: string,
+  saldo_disponivel: number,
+};
---
+++ src/types/Recurso.ts
@@ -0,0 +1,5 @@
+export type Recurso = {
+  id: number,
+  recurso: string,
+  saldo_disponivel: number,
+};
